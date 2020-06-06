8da99f1ae6d13bf2fa3e6dca56abecdf2c414d38
dragynmada <anygard@wisc.edu>
2020-06-05 19:27:47 -0500

    Add creation page and multiple selection feature. Very bugged out but I'm out of time today lol

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,47 @@
 function initializePage() {
     document.getElementById("homeContainer").style.display = "block";
     document.getElementById("generateContainer").style.display = "none";
+    document.getElementById("creationContainer").style.display = "none";
 }
 
 function hideHome() {
     document.getElementById("homeContainer").style.display = "none";
     document.getElementById("generateContainer").style.display = "block";
+}
+
+function hideGenerate() {
+    document.getElementById("generateContainer").style.display = "none";
+    document.getElementById("creationContainer").style.display = "block";
+}
+
+function createClassic() {
+    hideGenerate();
+}
+
+var grid = document.getElementById("sudoku-grid"),
+    selected = grid.getElementsByClassName("selected"),
+    selectMultiple = false;
+
+grid.addEventListener("mousedown", highlight);
+
+document.addEventListener("mousedown", function(event) {
+    if (event.ctrlKey)
+        selectMultiple = true;
+    else
+        selectMultiple = false;
+});
+
+function highlight(cell) {
+    selected = grid.getElementsByClassName("selected");
+    if (cell.target.className == "cell-data") { // check if cell isn't already selected
+        if (selectMultiple == false) {
+            for (var i = 0; i < selected.length; i++)
+                selected[i].className = "cell-data";
+            cell.target.className = "cell-data selected";
+        } else {
+            cell.target.className = "cell-data selected";
+        }   
+    } else { // cell is already selected, so deselect it
+        cell.target.className = "cell-data";        
+    }
 }
